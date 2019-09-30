5c0fcd78fb3c2e882478821dfc2d3b74b5dec7de
daksh37 <46132606+daksh37@users.noreply.github.com>
2019-09-30 16:51:40 +0530

    Update adhoc_utilization.sql

--- adhoc_utilization.sql
+++ adhoc_utilization.sql
@@ -23,3 +23,15 @@ group by plnm,rn;
 
 
 select visitsubtype from pd_visit_combined where payerid='1' group by visitsubtype;
+
+
+select count(*),lower(foo.chapter),lower(foo.planname) from (
+select distinct t2.visitid, 1 as ip_readmission_flag_ip_to_ip,t3.chapter,t3.planname from      
+        (select empi,visitstartdate,visitenddate,visitid,pcpregionname as chapter,planname
+        from pd_visit_combined
+         where visitsubtype='Acute Inpatient' and visitstartdate between '2018-07-01' and '2019-06-30') t3
+        left join         
+        (select empi,visitstartdate,visitenddate,visitid,diagnosisrelatedgroupcode,pcpservicelocationname as chapter
+        from pd_visit_combined where visitsubtype='Acute Inpatient' and visitstartdate between '2018-07-01' and '2019-06-30') t2       
+        on (t3.empi=t2.empi and (t2.visitstartdate-t3.visitenddate)<=30
+        and (t2.visitstartdate-t3.visitenddate)>0 and t3.visitid!=t2.visitid) where t2.visitid is not null) foo group by lower(foo.chapter),lower(foo.planname);
