33cabe0c1174219f34861c668696fdb25a261410
daksh37 <46132606+daksh37@users.noreply.github.com>
2019-10-16 11:27:27 +0530

    Update new_req_quality.sql

--- new_req_quality.sql
+++ new_req_quality.sql
@@ -4,3 +4,16 @@ select plan_name,count(*) from quality_measure_new where qualification_status='D
 and date_trunc ('month',measurement_date::date)='2019-06-01' and lower(time_window)='ytd' and payer_id='1'
 and member_empi in (select member_empi from quality_measure_new where qualification_status='NUM' and measure_id='cd66c533-a5fd-413d-b090-c78785cbadce' 
 and date_trunc ('month',measurement_date::date)='2018-12-01' and lower(time_window)='ytd') group by plan_name;
+
+
+select *
+from quality_measure_new qmo join  
+(select qo.* from quality_measure_new qm
+join quality_measure_operand qo on qm.key_operand=qo.key_operand
+where qm.qualification_status='NUM' and qm.measure_id='cd66c533-a5fd-413d-b090-c78785cbadce' 
+and date_trunc ('month',measurement_date::date)='2018-12-01' and lower(time_window)='ytd') foo on qmo.key_operand= foo.key_operand
+where qmo.qualification_status='DEN' and qmo.measure_id='cd66c533-a5fd-413d-b090-c78785cbadce'  
+and date_trunc ('month',measurement_date::date)='2019-06-01' and lower(time_window)='ytd' and payer_id='1' 
+and qmo.member_empi in (select member_empi from quality_measure_new where qualification_status='NUM' and measure_id='cd66c533-a5fd-413d-b090-c78785cbadce' 
+and date_trunc ('month',measurement_date::date)='2018-12-01' and lower(time_window)='ytd')
+;
