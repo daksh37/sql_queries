fed5c693bfbfd0c2c2c311c6727d2a82f299c7b6
daksh37 <46132606+daksh37@users.noreply.github.com>
2019-10-16 10:46:58 +0530

    Update new_req_quality.sql

--- new_req_quality.sql
+++ new_req_quality.sql
@@ -1,3 +1,5 @@
+
+//hb1ac
 select * from quality_measure_new where qualification_status='DEN' and measure_id='cd66c533-a5fd-413d-b090-c78785cbadce'  
 and date_trunc ('year',measurement_date::date)='2019-01-01' and time_window='ytd'
 and member_empi in (select member_empi from quality_measure_new where qualification_status='NUM' and measure_id='cd66c533-a5fd-413d-b090-c78785cbadce' 
