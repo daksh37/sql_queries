90a2e1be03752038f4b04a5f91b10bf32f0f67ce
daksh37 <46132606+daksh37@users.noreply.github.com>
2019-10-16 11:36:32 +0530

    Update new_req_quality.sql

--- new_req_quality.sql
+++ new_req_quality.sql
@@ -11,9 +11,10 @@ from quality_measure_new qmo join
 (select qo.* from quality_measure_new qm
 join quality_measure_operand qo on qm.key_operand=qo.key_operand
 where qm.qualification_status='NUM' and qm.measure_id='cd66c533-a5fd-413d-b090-c78785cbadce' 
-and date_trunc ('month',measurement_date::date)='2018-12-01' and lower(time_window)='ytd') foo on qmo.key_operand= foo.key_operand
+and date_trunc ('month',measurement_date::date)='2018-12-01' and lower(time_window)='ytd') foo on foo.member_empi=qmo.member_empi 
 where qmo.qualification_status='DEN' and qmo.measure_id='cd66c533-a5fd-413d-b090-c78785cbadce'  
 and date_trunc ('month',measurement_date::date)='2019-06-01' and lower(time_window)='ytd' and payer_id='1' 
 and qmo.member_empi in (select member_empi from quality_measure_new where qualification_status='NUM' and measure_id='cd66c533-a5fd-413d-b090-c78785cbadce' 
 and date_trunc ('month',measurement_date::date)='2018-12-01' and lower(time_window)='ytd')
 ;
+
