6893b3a25b2c1ee26cb38fc42c15b6d0cb2d6faa
daksh37 <46132606+daksh37@users.noreply.github.com>
2019-10-16 11:01:44 +0530

    Update new_req_quality.sql

--- new_req_quality.sql
+++ new_req_quality.sql
@@ -1,6 +1,6 @@
 
 //hb1ac
-select * from quality_measure_new where qualification_status='DEN' and measure_id='cd66c533-a5fd-413d-b090-c78785cbadce'  
-and date_trunc ('year',measurement_date::date)='2019-01-01' and time_window='ytd'
+select plan_name,count(*) from quality_measure_new where qualification_status='DEN' and measure_id='cd66c533-a5fd-413d-b090-c78785cbadce'  
+and date_trunc ('month',measurement_date::date)='2019-06-01' and lower(time_window)='ytd' and payer_id='1'
 and member_empi in (select member_empi from quality_measure_new where qualification_status='NUM' and measure_id='cd66c533-a5fd-413d-b090-c78785cbadce' 
-and date_trunc ('year',measurement_date::date)='2018-01-01' and time_window='ytd');
+and date_trunc ('month',measurement_date::date)='2018-12-01' and lower(time_window)='ytd') group by plan_name;
